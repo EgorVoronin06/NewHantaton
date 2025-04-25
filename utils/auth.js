d57a76f5a0027548ece327f735252653e97ab36f
EgorVoronin06 <voronine15@mail.ru>
2025-04-25 07:08:41 +0300

    Add files via upload

---
+++ utils/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.JWT_SECRET || 'your_jwt_secret',
+    { expiresIn: '7d' }
+  );
+};
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'Access token missing' });
+
+  jwt.verify(token, process.env.JWT_SECRET || 'your_jwt_secret', (err, user) => {
+    if (err) return res.status(403).json({ message: 'Invalid token' });
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = {
+  generateToken,
+  authenticateToken,
+};
